f2ad55b225fb84c9fcaf35d3c649f547daf1be9d
jonBarcus <barcus.j@gmail.com>
2014-04-28 16:46:16 -0400

    completed News_Spec.rb tests

--- spec/news_spec.rb
+++ spec/news_spec.rb
@@ -2,13 +2,39 @@ require 'spec_helper'
 
 describe News do
 
-  describe "The functions of a News object" do
+  describe "Get Headlines Method" do
 
-    it "get_headlines method should return an array" do
+    it "should return an array" do
       test = News.new("Queens")
       expect(test.get_headlines).to be_an(Array)
     end
 
+    it "should return an array with FIVE elements" do
+      test = News.new("Queens")
+      test_array = test.get_headlines
+      expect(test_array.length).to eq(5)
+    end
+
+  end
+
+  describe "Get URLs Method" do
+
+    it "should return an array" do
+      test = News.new("Queens")
+      expect(test.get_urls).to be_an(Array)
+    end
+
+    it "should return an array with FIVE elements" do
+      test = News.new("Queens")
+      test_array = test.get_urls
+      expect(test_array.length).to eq(5)
+    end
+
+    it "should return an array containing urls" do
+      test = News.new("Queens")
+      test_array = test.get_urls
+      expect(test_array[0]).to include("http://")
+    end
 
   end
 
