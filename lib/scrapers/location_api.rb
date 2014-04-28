e18ee62f8d7b3f7dcaf3b7ca44577738979f1d3e
ganondalf <cganon@gmail.com>
2014-04-28 11:58:30 -0400

    create location api module and all methods to grab location

--- lib/scrapers/location_api.rb
+++ lib/scrapers/location_api.rb
@@ -1,1 +1,20 @@
-weather_api.rb
+module Location
+  @response = HTTParty.get('http://ip-api.com/json')
+
+  def self.zipcode()
+    zip_code = @response["zip"]
+  end
+
+  def self.lat()
+    lat = @response["lat"]
+  end
+
+  def self.lon()
+    lon = @response["lon"]
+  end
+
+  def self.region()
+    region = @response["region"]
+  end
+
+end
