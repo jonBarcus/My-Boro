a34d95e7566b29cfd4e22daaf81711f7676e4621
ganondalf <cganon@gmail.com>
2014-04-29 10:56:59 -0400

    create user login and session controller functionality

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+
+  def create
+    user = User.find_by(email: params[:email])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_url
+    end
+  end
+
+  def destroy
+      @current_user = session[:user_id] = nil
+      flash[:notice] = "Goodbye! See you again soon."
+      redirect_to root_url
+  end
+
+
+end
