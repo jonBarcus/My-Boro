af91e3accd4648cf1ac488f9d605bcb54c0b0788
jonBarcus <barcus.j@gmail.com>
2014-04-29 13:46:17 -0400

    refactored restaurant_api
    to allow for giving it either a string such as Queens or the
    longitude and latitude.  I created three methods that will
    return arrays for Names, Addresses, and Ratings.

--- lib/scrapers/restaurant_api.rb
+++ lib/scrapers/restaurant_api.rb
@@ -1,25 +1,86 @@
-require 'httparty'
-require 'pry'
+# this class calls the Google Places API
+# to be used for providing our application with data
+# regarding restaurants in the NYC area
+# https://developers.google.com/places/documentation/search
+class Restaurants
 
-module Restaurant
+  # object instantiation will take either a string ("Queens")
+  # or a longitude and latitude (40.741061, -73.989699)
+  def initialize(*location)
 
-  @results_array = []
+    # because the application is only used for NYC
+    # it will test for specific strings unique to the
+    # names of the boroughs OR long/lat
+    # if the first location element is not one of the five
+    # boroughs, it will then assume long/lat was provided
+    if location[0] == "Queens" || location[0] == "Staten Island" || location[0] == "Bronx" || location[0] == "Manhattan" || location[0] == "Brooklyn"
+      location = location[0]
+      response = HTTParty.get("https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+#{location}&sensor=false&key=AIzaSyA8OfyjmSJdgy4py_PVNQbQ8a7mrwG7K8U&opennow")
+    else
+      latitude = location[0]
+      longitude = location[1]
+      response = HTTParty.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{latitude},#{longitude}&radius=500&types=food&sensor=false&types=restaurant&zagat_selected&key=AIzaSyA8OfyjmSJdgy4py_PVNQbQ8a7mrwG7K8U")
+    end
 
-  def self.top_10(latitude, longitude)
-
-    response = HTTParty.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{latitude},#{longitude}&radius=500&types=food&sensor=false&types=restaurant&zagat_selected&key=AIzaSyA8OfyjmSJdgy4py_PVNQbQ8a7mrwG7K8U")
-
+    # creating an empty array for the results from the API
+    # call to go.  We are also setting up a variable for iteration
+    @results_array = []
     i = 0
-    while @results_array.length < 10 do
+
+    # will put the first 10 results from the API call
+    # in to the results_array
+     while @results_array.length < 10 do
       @results_array << response["results"][i]
       i += 1
     end
 
   end
 
+  # this function returns the names of the restaurants
+  # in an array
+  def names
+    i = 0
+    name_array = []
+    while i < @results_array.length - 1 do
+      name_array << @results_array[i]["name"]
+      i += 1
+    end
+
+    return name_array
+
+  end
+
+  # this function returns the addresses of the
+  # restaurants in the array
+  def addresses
+    i = 0
+    address_array = []
+    while i < @results_array.length - 1 do
+      address_array << @results_array[i]["formatted_address"]
+      i += 1
+    end
+
+    return address_array
+
+  end
+
+  # this method returns an array made up of the Zagat
+  # ratings of the restaurants
+  def ratings
+    i = 0
+    rating_array = []
+    while i < @results_array.length - 1 do
+      rating_array << @results_array[i]["rating"]
+      i += 1
+    end
+
+    return rating_array
+
+  end
+
 end
 
-Restaurant.top_10(40.741061, -73.989699)
+# Restaurant.top_10(40.741061, -73.989699)
 
 # Revelant calls:
 # @response[0]["name"]
@@ -27,3 +88,4 @@ Restaurant.top_10(40.741061, -73.989699)
 # @response[0]["photos"]["photo_reference"]
 # @response[0]["vicinity"] returns formatted address
 
+# test = HTTParty.get("https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+#{location}&sensor=false&key=AIzaSyA8OfyjmSJdgy4py_PVNQbQ8a7mrwG7K8U&opennow")
