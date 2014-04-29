93786081b597c2e8b7d01880715d230ad555f8b9
ganondalf <cganon@gmail.com>
2014-04-29 19:09:52 -0400

    make edit

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -3,18 +3,16 @@ var totalHeader = {
 
   onReady: function(){
     $("#header").append("<img src='assets/PNGs/My-Boro_Header.png' class='img_header_logo'>");
-    $("#header").append("<img src='assets/PNGs/Sign_Up.png' class='img_header_session'>");
-    $("#header").append("<img src='assets/PNGs/Log_In.png' class='img_header_session'>");
-    $(".img_header_session").on("click", totalHeader.showSignUpForm);
+    $("#header").append("<img src='assets/PNGs/Sign_Up.png' class='img_new_session'>");
+    $("#header").append("<img src='assets/PNGs/Log_In.png' class='img_login_session'>");
+    $(".img_new_session").on("click", totalHeader.showSignUpForm);
     $("#signUpForm").hide();
     $("#subheader").hide();
     $(".category").hide();
   },
 
   showSignUpForm: function(event){
-    $("#signUpForm").slideDown(){
-      scroll
-    });
+    $("#signUpForm").slideDown();
   },
 
   chooseBureau: function(){
