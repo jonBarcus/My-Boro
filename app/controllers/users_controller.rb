6ae1f463479849530a6bfdf2dca4531865ab5671
ganondalf <cganon@gmail.com>
2014-04-29 11:34:22 -0400

    create user new create edit update functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,36 @@ class UsersController < ApplicationController
   def index
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path
+    else
+      render(:new)
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @user = User.find(@current_user.id)
+    @user.update(user_params)
+
+    if @user.save
+      redirect_to root_path
+    else
+      flash[:notice] = "User update was unsuccessful. Please try again!"
+      render(:edit)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 
 end
