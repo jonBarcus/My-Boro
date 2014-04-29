cbbe8f4e6269701b43a713f5e5270e4dfe5cf210
jonBarcus <barcus.j@gmail.com>
2014-04-29 11:31:39 -0400

    Updated news_spec.rb to use let(:test) {News.new(Queens)} instead of calling the API multiple times per test.

--- spec/news_spec.rb
+++ spec/news_spec.rb
@@ -3,15 +3,15 @@ require 'scrapers/news_api'
 
 describe News do
 
+  let(:test) {News.new("Queens")}
+
   describe "Get Headlines Method" do
 
     it "should return an array" do
-      test = News.new("Queens")
       expect(test.get_headlines).to be_an(Array)
     end
 
     it "should return an array with FIVE elements" do
-      test = News.new("Queens")
       test_array = test.get_headlines
       expect(test_array.length).to eq(5)
     end
@@ -21,18 +21,15 @@ describe News do
   describe "Get URLs Method" do
 
     it "should return an array" do
-      test = News.new("Queens")
       expect(test.get_urls).to be_an(Array)
     end
 
     it "should return an array with FIVE elements" do
-      test = News.new("Queens")
       test_array = test.get_urls
       expect(test_array.length).to eq(5)
     end
 
     it "should return an array containing urls" do
-      test = News.new("Queens")
       test_array = test.get_urls
       expect(test_array[0]).to include("http://")
     end
