c8a7c3c777d1d20a6d1e82e00563b775bee12be6
ganondalf <cganon@gmail.com>
2014-04-30 08:37:34 -0400

    make changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,15 @@
 class UsersController < ApplicationController
 
   def index
+    weather = Weather.new("Brooklyn")
+    response = {
+      current_temp: weather.current_temp,
+      current_weather: weather.current_weather,
+      current_wind: weather.wind,
+      current_humidity: weather.humidity
+    }
 
+    render :json => response
   end
 
   def create
