9eeef4125acce5f62cbf519e309a0670d4074633
ganondalf <cganon@gmail.com>
2014-04-30 01:01:14 -0400

    uncommented headers in heder.js

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -4,15 +4,15 @@ var totalHeader = {
   onReady: function(){
     $("body").append("#main_container")
     $("#main_container").append("#header")
-    // $("#header").append("<img src='assets/PNGs/My-Boro_Header.png' class='img_header_logo'>");
+    $("#header").append("<img src='assets/PNGs/My-Boro_Header.png' class='img_header_logo'>");
     $("#header").append("<img src='assets/PNGs/Sign_Up.png' class='img_new_session'>");
     $("#header").append("<img src='assets/PNGs/Log_In.png' class='img_login_session'>");
     $("#header").append("<div id='line'>");
     $("#signUpForm").hide();
     $(".img_new_session").on("click", totalHeader.showSignUpForm);
     $("form").on("submit", totalHeader.signUpUser);
-    // $("#subheader").hide();
-    // $(".category").hide();
+    $("#subheader").hide();
+    $(".category").hide();
   },
 
   showSignUpForm: function(event){
@@ -35,6 +35,7 @@ var totalHeader = {
             "password": password,
             "password_confirmation": password_confirmation }
           }).done(function(data){
+            debugger
             console.log(this);
           });
   }
