ccefa5a6b21131dd791aafceff150269c098fa3a
oshmanm <michael.oshman@gmail.com>
2014-05-01 17:36:22 -0400

    added seed file

--- db/schema.rb
+++ db/schema.rb
@@ -16,12 +16,6 @@ ActiveRecord::Schema.define(version: 20140430145047) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "categories", force: true do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "drinks", force: true do |t|
     t.integer  "user_id"
     t.string   "name"
@@ -29,17 +23,6 @@ ActiveRecord::Schema.define(version: 20140430145047) do
     t.string   "rating"
     t.datetime "created_at"
     t.datetime "updated_at"
-  end
-
-  create_table "favorites", force: true do |t|
-    t.string  "name"
-    t.string  "url"
-    t.integer "category_id"
-  end
-
-  create_table "favorites_users", force: true do |t|
-    t.integer "user_id"
-    t.integer "favorite_id"
   end
 
   create_table "movies", force: true do |t|
