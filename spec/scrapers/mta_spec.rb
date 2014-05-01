122f94cb91dbf9f15b7b583f8d50bb1ba4e6454c
jonBarcus <barcus.j@gmail.com>
2014-05-01 14:12:26 -0400

    mostly done with mta_spec
    not sure how to test different status messages in the
    status array, so I commented it out.  We will address
    later.

--- spec/scrapers/mta_spec.rb
+++ spec/scrapers/mta_spec.rb
@@ -24,6 +24,33 @@ describe MTA do
       expect(test_array.length).to eq(6)
     end
 
+    it "should include the 7 line" do
+      expect(new_mta.queens_lines).to include("7")
+    end
+
+    it "should include the NQR" do
+      expect(new_mta.queens_lines).to include("NQR")
+    end
+
+    it "should include the G line" do
+      expect(new_mta.queens_lines).to include("G")
+    end
+
+    it "should include the AE lines" do
+      expect(new_mta.queens_lines).to include("AE")
+    end
+
+    it "should include the FM lines" do
+      expect(new_mta.queens_lines).to include("FM")
+    end
+
+    it "should include the JZ lines" do
+      expect(new_mta.queens_lines).to include("JZ")
+    end
+
+    # it "queens_status should return an array with values of SERVICE CHANGE/PLANNED WORK/DELAYS/GOOD SERVICE" do
+    #   expect(new_mta.queens_status).to include("GOOD SERVICE" || "PLANNED WORK" || "DELAYS" || "SERVICE CHANGE" || "SANDY REROUTE")
+    # end
   end
 
   describe "Bronx" do
@@ -32,10 +59,26 @@ describe MTA do
       expect(new_mta.bronx_lines).to be_an(Array)
     end
 
-    it "bronx_lines should return an array of 2 elements" do
+    it "bronx_lines should return an array of 3 elements" do
       test_array = new_mta.bronx_lines
       expect(test_array.length).to eq(3)
     end
+
+    it "should include the 456" do
+      expect(new_mta.bronx_lines).to include("456")
+    end
+
+    it "should include the BD" do
+      expect(new_mta.bronx_lines).to include("BD")
+    end
+
+    it "should include the 2 line" do
+      expect(new_mta.bronx_lines).to include("2")
+    end
+
+    # it "bronx_status should return an array with values of SERVICE CHANGE/PLANNED WORK/DELAYS/GOOD SERVICE" do
+    #   expect(new_mta.bronx_status).to include("GOOD SERVICE" || "PLANNED WORK" || "DELAYS" || "SERVICE CHANGE" || "SANDY REROUTE")
+    # end
 
   end
 
@@ -50,6 +93,41 @@ describe MTA do
       expect(test_array.length).to eq(8)
     end
 
+    it "should include the ACE" do
+      expect(new_mta.manhattan_lines).to include("ACE")
+    end
+
+    it "should include the 123" do
+      expect(new_mta.manhattan_lines).to include("123")
+    end
+
+    it "should include the NQR" do
+      expect(new_mta.manhattan_lines).to include("NQR")
+    end
+
+    it "should include the 456" do
+      expect(new_mta.manhattan_lines).to include("456")
+    end
+
+    it "should include the 7" do
+      expect(new_mta.manhattan_lines).to include("7")
+    end
+
+    it "should include the BDFM" do
+      expect(new_mta.manhattan_lines).to include("BDFM")
+    end
+
+    it "should include the L" do
+      expect(new_mta.manhattan_lines).to include("L")
+    end
+
+    it "should include the S" do
+      expect(new_mta.manhattan_lines).to include("S")
+    end
+
+    # it "manhattan_status should return an array with values of SERVICE CHANGE/PLANNED WORK/DELAYS/GOOD SERVICE" do
+    #   expect(new_mta.manhattan_status).to include("GOOD SERVICE" || "PLANNED WORK" || "DELAYS" || "SERVICE CHANGE" || "SANDY REROUTE")
+    # end
   end
 
   describe "Staten Island" do
@@ -61,6 +139,14 @@ describe MTA do
       test_array = new_mta.staten_island_lines
       expect(test_array.length).to eq(1)
     end
+
+    it "should include the SIR" do
+      expect(new_mta.staten_island_lines).to include("SIR")
+    end
+
+    # it "staten_island_status should return an array with values of SERVICE CHANGE/PLANNED WORK/DELAYS/GOOD SERVICE" do
+    #   expect(new_mta.staten_island_status).to include("GOOD SERVICE" || "PLANNED WORK" || "DELAYS" || "SERVICE CHANGE" || "SANDY REROUTE")
+    # end
 
   end
 
@@ -75,6 +161,41 @@ describe MTA do
       expect(test_array.length).to eq(8)
     end
 
+    it "should include the NQR" do
+      expect(new_mta.brooklyn_lines).to include("NQR")
+    end
+
+    it "should include the BDFM" do
+      expect(new_mta.brooklyn_lines).to include("BDFM")
+    end
+
+    it "should include the G" do
+      expect(new_mta.brooklyn_lines).to include("G")
+    end
+
+    it "should include the JZ" do
+      expect(new_mta.brooklyn_lines).to include("JZ")
+    end
+
+    it "should include the AC" do
+      expect(new_mta.brooklyn_lines).to include("AC")
+    end
+
+    it "should include the 3" do
+      expect(new_mta.brooklyn_lines).to include("3")
+    end
+
+    it "should include the 45" do
+      expect(new_mta.brooklyn_lines).to include("45")
+    end
+
+    it "should include the L" do
+      expect(new_mta.brooklyn_lines).to include("L")
+    end
+
+    # it "brooklyn_status should return an array with values of SERVICE CHANGE/PLANNED WORK/DELAYS/GOOD SERVICE" do
+    #   expect(new_mta.brooklyn_status).to include("GOOD SERVICE" || "PLANNED WORK" || "DELAYS" || "SERVICE CHANGE" || "SANDY REROUTE")
+    # end
   end
 
 end
